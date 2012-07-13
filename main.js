823cdde1521906f4702b8ef82230bd13709bf0d3
Sean Gransee <sean.gransee@gmail.com>
2012-07-13 09:34:08 -0400

    made it prettier

--- main.js
+++ main.js
@@ -1,34 +1,90 @@
-function checkAll() {
+WebFontConfig = {
+    google: { families: [ 'PT+Sans::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
 
-	var overlay = '<div class="overlay"><h1>GTFO</h1></div>'
+var updateStopCount = function() {
+	var now = new Date().getTime();
+	var future = new Date(now + 3*60000).getTime();
+	localStorage.setItem("FBstop", future);
+};
 
-	var notif = false;
+var countdown = {
+	show: function() {
+		$('#countdown').fadeIn('slow');
+	},
+	hide: function() {
+		$('#countdown').fadeOut('slow');
+	}
+};
 
-	var minutes = 3;
-	var now = new Date();
-	var future = new Date(now.getTime() + minutes*60000);
+var overlay = {
+	show: function() {
+		$('#overlay, #message').fadeIn('slow');
+		$('body').css('overflow', 'hidden');
+	},
+	hide: function() {
+		$('#overlay, #message').fadeOut('slow');
+		$('body').css('overflow', 'auto');
+	}
+};
 
-	var path = window.location.pathname.replace(/\//g,"")
-
-	console.log(path);
-
-	var stop = localStorage.getItem("FBstop");
-
-	$('.jewelCount span').each(function() { if ($(this).text() > 0) notif = true; });
-
-	if (notif) {
-		localStorage.setItem("FBstop", future);
+function loop() {
+	var login = document.title.indexOf('Log In') > -1;
+	var messages = document.title.split(' ').reverse()[0] == "Messages";
+	if (login || messages) {
+		console.log('login or messages page');
+		countdown.hide();
+		overlay.hide();
 	} else {
-		if (stop < now && path != "messages") {
-			//$('body').html('<h1>GTFO</h1><p>You can visit <a target="_top" href="https://www.facebook.com/messages/">Facebook Messages</a> to talk to people.</p>');
-			$('body').append(overlay)
+		console.log('not login or messages page');
+		var notifications = false;
+		$('.jewelCount span').each(function() { if ($(this).text() > 0) { notifications = true; } });
+		if (notifications) {
+			console.log('notifications found');
+			updateStopCount();
+			countdown.hide();
+			overlay.hide();
+		} else {
+			console.log('no notifications found');
+			var stop = parseInt(localStorage.getItem("FBstop"), 10);
+			var now = new Date().getTime();
+			if (stop < now) {
+				console.log('stop < now');
+				overlay.show();
+			} else {
+				console.log('stop > now');
+				overlay.hide();
+				countdown.show();
+			}
 		}
 	}
 }
 
-checkAll();
+loop();
+
+setInterval(function() {
+	loop();
+}, 5000);
 
 
-$('a').click(function() {
-	window.setTimeout("checkAll()", 1000);
-});
+var popup = '\
+<div id="overlay">									\
+</div>															\
+<div id="message">    							\
+	<h1>Can\'t let you do that!</h1>  \
+	<p>You don\'t have any messages or notifications, which makes Facebook a waste of your time right now. You may still use <a href="http://www.facebook.com/messages">Facebook messages</a>.</p> \
+</div>    													\
+';
+
+$('body').append(popup);
+$('body').append('<div id="countdown">blah countdown</div>');
+
