345c2ab71f366b276e56b51eb76f140ccd4abff4
Sean Gransee <sean.gransee@gmail.com>
2013-03-13 00:48:46 -0500

    build script and new manifest

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+zip -r extension.zip icon.png \
+                     icon128.png \
+                     icon48.png \
+                     jquery.min.js \
+                     main.js \
+                     manifest.json \
+                     README.md \
+                     screenshot.png \
+                     styles.css
